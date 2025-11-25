8ec97213464c0ecda0865328d09c202000b37be8
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2025-11-24 19:48:39 -0500

    Create niftybotv2.js

---
+++ js/niftybotv2.js
@@ -0,0 +1,7 @@
+// Config
+const PROXY_URL = "https://nifty-proxy-566869872467.us-east5.run.app/niftybotv2/chat";
+
+// DOM
+const messageDisplay = document.getElementById("message-display");
+const userInput = document.getElementById("user-input");
+const sendButton = document.getElementById("send-button");
