47dd86e6f736e20129574bd36711427a1de2a149
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2025-11-26 14:11:39 -0500

    Introduce error in proxy url

--- js/niftybotv2.js
+++ js/niftybotv2.js
@@ -1,7 +1,7 @@
 // ---------------------------
 // Config
 // ---------------------------
-const PROXY_URL = "https://nifty-proxy-566869872467.us-east5.run.app/niftybotv2/chat";
+const PROXY_URL = "https://nifty-proxy-566869872467.us-east5.run.app/niftybotv2e/chat";
 
 // ---------------------------
 // State
