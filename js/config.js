6278ed5e4c6d74655e6f8eda5cdff583bfa3e710
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2026-02-10 18:42:15 -0500

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// API configuration & Oauth
+export const CONFIG = {
+  PROXY_URL: 'https://dashboard-proxy-59184217758.us-east5.run.app',
+  GOOGLE_CLIENT_ID: 'YOUR_CLIENT_ID.apps.googleusercontent.com'
+};
