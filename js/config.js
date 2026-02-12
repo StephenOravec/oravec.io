7e2c46ea8640753804a8025713b7732137791cb6
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2026-02-12 14:06:34 -0500

    Add client ID

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-// API configuration & Oauth
+// reverse proxy & OAuth
 export const CONFIG = {
   PROXY_URL: 'https://dashboard-proxy-59184217758.us-east5.run.app',
-  GOOGLE_CLIENT_ID: 'YOUR_CLIENT_ID.apps.googleusercontent.com'
+  GOOGLE_CLIENT_ID: '59184217758-7emaj1pv2b50bnq8d3fbb4idlrjsvp36.apps.googleusercontent.com'
 };
