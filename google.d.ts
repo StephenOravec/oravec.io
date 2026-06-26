5b9fedb782cfe6d592cf3676235439f5c1e72765
Stephen Oravec <81935688+StephenOravec@users.noreply.github.com>
2026-06-26 18:55:02 -0400

    Initial commit

---
+++ google.d.ts
@@ -0,0 +1,26 @@
+interface GoogleOAuth2CodeClient {
+  requestCode(): void;
+}
+
+interface GoogleOAuth2 {
+  initCodeClient(config: {
+    client_id: string;
+    scope: string;
+    ux_mode: string;
+    callback: (response: { code?: string }) => void;
+  }): GoogleOAuth2CodeClient;
+}
+
+interface GoogleAccounts {
+  oauth2: GoogleOAuth2;
+}
+
+interface Google {
+  accounts: GoogleAccounts;
+}
+
+interface Window {
+  google?: Google;
+}
+
+declare const google: Google;
